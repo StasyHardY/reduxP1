1585f5f2946cad21b4adc7975a6e522ea9f82874
Max <mklevogin@mail.ru>
2022-07-08 00:02:43 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { titleChanged, taskDeleted, completeTask, loadTasks, getTasks, getTasksLoadingStatus, createTask, }  from "./store/task";
+import configureStore from "./store/store";
+import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
+
+const store = configureStore();
+
+const App = () => {
+  const state = useSelector(getTasks());
+  const isLoading = useSelector(getTasksLoadingStatus());
+  const error = useSelector(getError());
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTasks());
+  }, []);
+
+  const taskCreate = () => {
+    dispatch(createTask({
+      completed:false,
+      title:"some title"
+    }));
+  };
+
+  const changeTitle = (taskId) => {
+    dispatch(titleChanged(taskId));
+  };
+  const deleteTask = (taskId) => {
+    dispatch(taskDeleted(taskId));
+  };
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <>
+      <h1> App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Completed: ${el.completed}`}</p>
+            <button onClick={() => dispatch(completeTask(el.id))}> Complete </button>
+            <button onClick={() => changeTitle(el.id)}> Change title </button>
+            <button onClick={() => deleteTask(el.id)}> Delete </button>
+            <button onClick={() => taskCreate()}> Add task </button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
